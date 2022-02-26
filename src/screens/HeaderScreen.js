8f29bd0cd8542ad51cb40c5aeaa78ca8443315c0
Salome-Hernandez-Adolfo-Angel <64882685+Salome-Hernandez-Adolfo-Angel@users.noreply.github.com>
2022-02-26 12:11:55 -0600

    Modificaciones menores

--- src/screens/HeaderScreen.js
+++ src/screens/HeaderScreen.js
@@ -1,51 +1,43 @@
-//Este screen es solo la parte superior de la mayoria de las Screens, se importó para que 
+//Este screen es solo la parte superior de la mayoria de las Screens, se importó para que
 //sea reusable.
-import {View, Text, ImageBackground, Image} from 'react-native';
+import {View, Image} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const HeaderScreen = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-    <View
-      style={{
-        backgroundColor: 'rgba(34,255,0,0.9)',
-        height: 300,
-        width: 400,
-        top: '-40%',
-        borderRadius: 200,
-        left: 0,
-      }}>
-      <View style={{borderRadius:200, width: 400, height: 300, flex: 1}}>
-        <Image
-          source={require('../../assets/img/85246-h.jpg')}
-          resizeMode= 'cover'
-          style={{width: 400, height: 300, opacity: 0.4, borderRadius: 200}}
-        />
-      </View>
-      <Icon
-        name="info-circle"
-        size={30}
-        style={{color: 'black', left: '85%', marginVertical: 90}}></Icon>
-      <View>
-        <Image
-          source={require('../../assets/img/logo_negro.png')}
-          style={{width: 140, height: 140, marginVertical: -155}}
-        />
-      </View>
-      <Text
+      <View
         style={{
-          color: 'white',
-          fontSize: 50,
-          marginVertical: 10,
-          left: 90,
-          fontWeight: 'bold',
+          backgroundColor: 'rgba(34,255,0,0.9)',
+          height: 300,
+          width: 400,
+          top: '-40%',
+          borderRadius: 200,
+          left: 0,
         }}>
-        GreenLife
-      </Text>
+        <View style={{borderRadius: 200, width: 400, height: 300, flex: 1}}>
+          <Image
+            source={require('../../assets/img/85246-h.jpg')}
+            resizeMode="cover"
+            style={{width: 400, height: 300, opacity: 0.4, borderRadius: 200}}
+          />
+        </View>
+        <View style={{left: '85%', top: -180}}>
+        <Icon
+          name="info-circle"
+          size={30}
+          style={{color: 'black'}}></Icon>
+          </View>
+        <View>
+          <Image
+            source={require('../../assets/img/logo_negro.png')}
+            style={{width: 200, height: 200, marginVertical: -220, marginHorizontal: 95}}
+          />
+        </View>
+      </View>
     </View>
-  </View>
-  )
-}
+  );
+};
 
-export default HeaderScreen
+export default HeaderScreen;
