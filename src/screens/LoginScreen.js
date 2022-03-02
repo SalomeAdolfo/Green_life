fffc3e1bd41b167ba0ba6aab01e893565a55bf06
Salome-Hernandez-Adolfo-Angel <64882685+Salome-Hernandez-Adolfo-Angel@users.noreply.github.com>
2022-03-02 10:56:32 -0600

    Pantalla LogIn

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,17 +1,17 @@
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
 import React from 'react';
 import HeaderScreen from './HeaderScreen';
-import {Card} from 'react-native-elements';
+import {Card, Button, colors} from 'react-native-elements';
 import {Fumi} from 'react-native-textinput-effects';
 import Icons from 'react-native-vector-icons/FontAwesome';
 
 const LoginScreen = () => {
   return (
-    <View>
-      <View style={{flex: 1, marginVertical: 70}}>
+    <ScrollView>
+      <View style={{marginVertical: 30}}>
         <HeaderScreen />
       </View>
-      <View style={{marginVertical: 90}}>
+      <View style={{marginVertical: -140, height: 550}}>
         <Card
           containerStyle={{
             height: 360,
@@ -22,7 +22,16 @@ const LoginScreen = () => {
             borderRadius: 20,
             shadowColor: '#00FF40',
           }}>
-          <View style={{marginVertical: '30%'}}>
+          <Card.Title
+            style={{
+              color: '#04B404',
+              fontSize: 40,
+              fontWeight: 'normal',
+              fontFamily: 'sans-serif',
+            }}>
+            Log In
+          </Card.Title>
+          <View style={{}}>
             <Fumi
               label={'Username'}
               iconClass={Icons}
@@ -41,12 +50,28 @@ const LoginScreen = () => {
               iconSize={30}
               iconWidth={50}
               inputPadding={18}
+              secureTextEntry={true}
               style={{}}
             />
+            <Button type='clear' title="Not yet a member? Sign Up" titleStyle={{fontSize: 14}} buttonStyle={{left: '150%'}}
+            />
+            <View style={{marginVertical: 20}}>
+            <Button type="outline"
+            title="Submit"
+            buttonStyle={{
+              borderRadius: 15,
+              borderColor: '#D7DF01',
+              flexDirection: 'row',
+              width: '80%',
+              alignSelf: 'center',
+              borderWidth: 1.5,
+            }} 
+            titleStyle={{color: "#04B404", fontSize:18 }}/>
+            </View>
           </View>
         </Card>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
