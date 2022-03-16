7e26b32cd3228f2c0036f8a7b1d8ce78d5947029
Salome-Hernandez-Adolfo-Angel <64882685+Salome-Hernandez-Adolfo-Angel@users.noreply.github.com>
2022-03-15 21:15:16 -0600

    Eliminación de botones e implementación de carousel

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,85 +1,76 @@
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, FlatList, Text, ScrollView, SafeAreaView} from 'react-native';
 import React from 'react';
 import HeaderScreen from './HeaderScreen';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {Button} from 'react-native-elements';
+import {Button, Card} from 'react-native-elements';
+
+const data = [
+  {
+    titulo: 'Lechuga',
+    imagen:
+      'https://th.bing.com/th/id/OIP.f5Gn3smjpbYTmNtYjEHnmQHaHa?pid=ImgDet&rs=1',
+    descripcion: 'Lechuga romana 1',
+  },
+  {
+    titulo: 'Lechuga',
+    imagen:
+      'https://th.bing.com/th/id/OIP.f5Gn3smjpbYTmNtYjEHnmQHaHa?pid=ImgDet&rs=1',
+    descripcion: 'Lechuga romana 2',
+  },
+  {
+    titulo: 'Lechuga',
+    imagen:
+      'https://th.bing.com/th/id/OIP.f5Gn3smjpbYTmNtYjEHnmQHaHa?pid=ImgDet&rs=1',
+    descripcion: 'Lechuga romana 3',
+  },
+  {
+    titulo: 'Lechuga',
+    imagen:
+      'https://th.bing.com/th/id/OIP.f5Gn3smjpbYTmNtYjEHnmQHaHa?pid=ImgDet&rs=1',
+    descripcion: 'Lechuga romana 4',
+  },
+];
+
+const Item = ({titulo, imagen, descripcion}) => (
+  <View style={{height: 300}}>
+    <Card containerStyle={{...styles.Cards}}>
+      <Card.Title>{titulo}</Card.Title>
+      <Card.Image source={{uri: imagen}}></Card.Image>
+      <Text style={{color: 'black', alignSelf:'center'}}>{descripcion}</Text>
+    </Card>
+  </View>
+);
 
 const HomeScreen = () => {
+  const renderItem = ({item}) => (
+    <Item
+      titulo={item.titulo}
+      imagen={item.imagen}
+      descripcion={item.descripcion}
+    />
+  );
   return (
     <View>
-      <View style={{marginVertical: 30}}>
+      <View>
         <HeaderScreen />
       </View>
-      <View
-        style={{
-          marginVertical: 380,
-          flexDirection: 'row',
-          height: 65,
-          position: 'absolute',
-          left: 10,
-          right: 10,
-        }}>
-        <View style={{width: '50%', paddingHorizontal: 10}}>
-          <Button
-            title="Offers"
-            type="solid"
-            buttonStyle={{
-              ...styles.botones,
-            }}
-            titleStyle={{
-              ...styles.botones_texto,
-            }}
-          />
-        </View>
-        <View style={{width: '50%', paddingHorizontal: 10}}>
-          <Button
-            title="News"
-            type="solid"
-            buttonStyle={{
-              ...styles.botones,
-            }}
-            titleStyle={{
-              ...styles.botones_texto,
-            }}
-          />
-        </View>
-      </View>
-      <View
-        style={{
-          height: 65,
-          marginVertical: 480,
-          position: 'absolute',
-          left: 20,
-          right: 20,
-        }}>
-        <Button
-          title="Administer"
-          type="solid"
-          buttonStyle={{
-            ...styles.botones,
-          }}
-          titleStyle={{
-            ...styles.botones_texto,
-          }}
-        />
-      </View>
+      <FlatList
+        style={{marginVertical: '20%'}}
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.descripcion}
+        horizontal
+      />
     </View>
   );
 };
 const styles = StyleSheet.create({
-  botones: {
-    width: '100%',
-    height: 70,
+  Cards: {
+    width: 200,
+    height: 250,
     borderRadius: 20,
-    backgroundColor: '#26de81',
-  },
-  botones_texto: {
-    color: 'white',
-    fontFamily: 'Arial',
-    fontSize: 25,
-    fontWeight: 'bold',
-    fontStyle: 'normal',
-  },
+    elevation: 5,
+    shadowColor: 'green',
+  }
 });
-
 export default HomeScreen;
